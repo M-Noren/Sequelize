b3c78d79093e3a3299f1f3c05698ad5aee4d8c65
Martin Noren <55893214+M-Noren@users.noreply.github.com>
2021-04-08 22:16:13 -0400

    Fixed script for loading tables and stylized CSS

--- public/script.js
+++ public/script.js
@@ -1,48 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Oluwakorede Noren's Lab 5</title>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-    crossorigin=""/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
-    <link rel="stylesheet" type="text/css" href="styles.css" />
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-    crossorigin=""></script>
-  </head>
+// Retrieve dining halls
+async function dataHandler() {
+  const endpoint = '/api/dining';
+  console.log(endpoint);
+  const request = await fetch(endpoint);
+  const dininghalls = await request.json();
+  return dininghalls;
 
-  <body>
+}
 
-    <section class="section">
-      <div class="container columns is-max-widescreen">
-        <div class="column info">
-          <h1 class="title">Assignment 1</h1>
-          <h2 class="subtitle">Oluwakorede Noren's Lab 5</h2>
-          <div class="block">
-            <form id="search-form" action="" class="block form">
-              <input
-                type=""
-                class="search"
-                id="userinput"
-                placeholder="Restaurant type or Zip Code"
-              />
-            </form>
-            <div class="reply-box block">
-              <div class="reply-message"></div>
-              <ul class="target-list"></ul>
-            </div>
-          </div>
+// This function loads the dining halls into the table
+function loadTableData(data) {
+  const table = document.getElementById("DiningHalls");
+  const row = table.insertRow();
+  const hall = row.insertCell(0);
+  hall.innerHTML = data.hall_name;
+  const location = row.insertCell(1);
+  location.innerHTML = data.hall_address;
+}
 
-        </div>
+// Once the request is successfully retrieved, pass the data into our table function
+dataHandler().then(dininghalls => {
+  console.log(dininghalls.data[0]);
 
-        <div class="column">
-          <div class="box" id="mapid">Map</div>
-        </div>
-      </div>
-    </section>
-  </body>
-  <script src="script.js"></script>
-</html>
+  for (i = 0; i < dininghalls.data.length; i += 1) {
+    loadTableData(dininghalls.data[i]);
+  }
+})
--- server.js
+++ server.js
@@ -28,3 +28,4 @@ async function bootServer() {
 }
 
 bootServer();
+
