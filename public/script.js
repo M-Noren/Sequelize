342ba588c997e3e4809b63dc35b973d1692d20e3
Martin Noren <55893214+M-Noren@users.noreply.github.com>
2021-04-15 09:49:46 -0400

    Added comments and centered page

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 // Retrieve dining halls
 async function dataHandler() {
+  // Retrieve dining, meals and macros endpoints
   const endpoint1 = '/api/dining';
   const request1 = await fetch(endpoint1);
   const dininghalls = await request1.json();
@@ -16,17 +17,16 @@ async function dataHandler() {
   console.log(meals);
   console.log(macros);
 
+  // This function extracts the datapoints we need from the data collected above
   function getData(mealss, macross, macro) {
     data = [];
     for (i = 0; i < 46; i += 1) {
-      data.push({ label: mealss[i].meal_name, y: eval('macross[i].' + macro)});
+      data.push({ label: mealss[i].meal_name, y: eval(`macross[i].${macro}`)});
     }
     return data;
   }
 
-  
-  console.log(getData(meals, macros, 'calories'));
-
+  // Create chart variable
   const chart = new CanvasJS.Chart('chartContainer', {
     animationEnabled: true,
     title: {
@@ -37,7 +37,7 @@ async function dataHandler() {
       interval: 1
     },
     axisY: {
-      labelFontSize: 10,
+      labelFontSize: 10
     },
     toolTip: {
       shared: true
@@ -91,6 +91,7 @@ async function dataHandler() {
 
   chart.render();
 
+  // Render chart
   function toggleDataSeries(e) {
     if (typeof (e.dataSeries.visible) === 'undefined' || e.dataSeries.visible) {
       e.dataSeries.visible = false;
@@ -100,31 +101,20 @@ async function dataHandler() {
     chart.render();
   }
 
+  // This function loads the dining halls into the table
+  function loadTableData(data) {
+    const table = document.getElementById('DiningHalls');
+    const row = table.insertRow();
+    const hall = row.insertCell(0);
+    hall.innerHTML = data.hall_name;
+    const location = row.insertCell(1);
+    location.innerHTML = data.hall_address;
+  }
 
-
-
-
+  for (i = 0; i < dininghalls.data.length; i += 1) {
+    loadTableData(dininghalls.data[i]);
+  }
 }
+
+// Load our page
 dataHandler();
-
-// // This function loads the dining halls into the table
-// function loadTableData(data) {
-//   const table = document.getElementById("DiningHalls");
-//   const row = table.insertRow();
-//   const hall = row.insertCell(0);
-//   hall.innerHTML = data.hall_name;
-//   const location = row.insertCell(1);
-//   location.innerHTML = data.hall_address;
-// }
-
-// // Once the request is successfully retrieved, pass the data into our table function
-// dataHandler().then(dininghalls => {
-//   console.log(dininghalls.data[0]);
-
-//   for (i = 0; i < dininghalls.data.length; i += 1) {
-//     loadTableData(dininghalls.data[i]);
-//   }
-// })
-
-window.onload = () => {
-};
